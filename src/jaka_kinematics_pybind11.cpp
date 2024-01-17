a43460d9b9fc277c325e5716b73c8199ae203529
cqzw555 <cqzw555@163.com>
2024-01-17 15:24:01 +0800

    修改变量生命周期

--- src/jaka_kinematics_pybind11.cpp
+++ src/jaka_kinematics_pybind11.cpp
@@ -4,12 +4,10 @@
 #include <assert.h>
 
 PYBIND11_MODULE(jaka_kinematics_pybind11, m)
-{   
+{
     pybind11::class_<jaka_kinematics>(m, "jaka_kinematics")
         .def(pybind11::init())
         .def(pybind11::init<const std::vector<double>>())
-        .def("forward",&jaka_kinematics::forward_vector,pybind11::arg("joints"),"forward kinematics function")
-        .def("inverse",&jaka_kinematics::inverse_vector,pybind11::arg("matrix"),pybind11::arg("q6_des")=0,"inverse kinematics function");
-
-    
+        .def("forward", &jaka_kinematics::forward_vector, pybind11::arg("joints"), "forward kinematics function", pybind11::return_value_policy::reference)
+        .def("inverse", &jaka_kinematics::inverse_vector, pybind11::arg("matrix"), pybind11::arg("q6_des") = 0, "inverse kinematics function", pybind11::return_value_policy::reference);
 }
